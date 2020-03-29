accf0c4fd8c3f1366550510c6fe9cdbdf9e6bcbd
Ewan <git@ewan.emccartney.co.uk>
2020-03-29 20:52:30 +0100

    Get things building again.

--- E.ExploreDeezer.UWP/Views/WhatsNewView.xaml.cs
+++ E.ExploreDeezer.UWP/Views/WhatsNewView.xaml.cs
@@ -14,7 +14,6 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 using E.ExploreDeezer.Core;
-using E.ExploreDeezer.UWP.Extensions;
 using E.ExploreDeezer.Core.ViewModels;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
