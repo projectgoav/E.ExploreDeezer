ed9b6a724f7c824116b92338244a364d30254b42
Ewan <git@ewan.emccartney.co.uk>
2020-04-16 15:34:05 +0100

    Updated E.Deezer to allow us paged responsed within album tracklists. Converted the tracklist to be a paged observable collection.

--- E.ExploreDeezer.Core/Common/Tracklist/TracklistDataController.cs
+++ E.ExploreDeezer.Core/Common/Tracklist/TracklistDataController.cs
@@ -7,7 +7,6 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using E.Deezer;
-using E.Deezer.Api;
 
 using E.ExploreDeezer.Core.Util;
 using E.ExploreDeezer.Core.ViewModels;
@@ -40,13 +39,11 @@ namespace E.ExploreDeezer.Core.Common
     internal class TracklistDataController : ITracklistDataController,
                                              IDisposable
     {
-        private const uint MAX_PLAYLIST_TRACKS = 1024;
-
         private readonly IDeezerSession session;
         private readonly UpdatableFetchState fetchState;
         private readonly UpdatableFetchState completeItemFetchState;
         private readonly ResetableCancellationTokenSource tokenSource;
-        private readonly FixedSizeObservableCollection<ITrackViewModel> tracklist;
+        private readonly PagedObservableCollection<ITrackViewModel> tracklist;
 
 
         public TracklistDataController(IDeezerSession session)
@@ -57,7 +54,7 @@ namespace E.ExploreDeezer.Core.Common
             this.completeItemFetchState = new UpdatableFetchState();
 
             this.tokenSource = new ResetableCancellationTokenSource();
-            this.tracklist = new FixedSizeObservableCollection<ITrackViewModel>();
+            this.tracklist = new PagedObservableCollection<ITrackViewModel>();
 
             this.ItemId = 0;
             this.Type = ETracklistType.Unknown;
@@ -106,15 +103,11 @@ namespace E.ExploreDeezer.Core.Common
         {
             this.tokenSource.Reset();
 
-            this.tracklist.ClearContents();
-
             this.fetchState.SetLoading();
             this.completeItemFetchState.SetLoading();
 
             this.CompleteAlbum = null;
             this.CompletePlaylist = null;
-
-            Task<IEnumerable<ITrack>> tracksTask = null;
 
             switch(this.Type)
             {
@@ -138,7 +131,36 @@ namespace E.ExploreDeezer.Core.Common
 
                                        }, this.tokenSource.Token, TaskContinuationOptions.NotOnCanceled | TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
 
-                    tracksTask = this.session.Albums.GetAlbumTracks(this.ItemId, this.tokenSource.Token);
+                    this.tracklist.SetFetcher((start, count, ct) => this.session.Albums.GetAlbumTracks(this.ItemId, ct, (uint)start, (uint)count)
+                                                                                       .ContinueWith<IEnumerable<ITrackViewModel>>(t =>
+                                                                                       {
+                                                                                           if (t.IsFaulted)
+                                                                                           {
+                                                                                               this.fetchState.SetError();
+
+                                                                                               var ex = t.Exception.GetBaseException();
+                                                                                               System.Diagnostics.Debug.WriteLine($"Failed to get album tracklist. {ex}");
+
+                                                                                               throw ex;
+                                                                                           }
+
+                                                                                           var items = t.Result.Select(x => new TrackViewModel(x, ETrackLHSMode.Number));
+
+                                                                                           bool hasContents = this.tracklist.Count > 0 || items.Any();
+
+                                                                                           if (hasContents)
+                                                                                           {
+                                                                                               this.fetchState.SetAvailable();
+                                                                                           }
+                                                                                           else
+                                                                                           {
+                                                                                               this.fetchState.SetEmpty();
+                                                                                           }
+
+                                                                                           return items;
+
+                                                                                       }, ct, TaskContinuationOptions.NotOnCanceled | TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default));
+
                     break;
 
                 case ETracklistType.Playlist:
@@ -161,43 +183,42 @@ namespace E.ExploreDeezer.Core.Common
 
                                           }, this.tokenSource.Token, TaskContinuationOptions.NotOnCanceled | TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
 
-                    tracksTask = this.session.Playlists.GetTracks(this.ItemId, this.tokenSource.Token, count: MAX_PLAYLIST_TRACKS);
+
+                    this.tracklist.SetFetcher((start, count, ct) => this.session.Playlists.GetTracks(this.ItemId, ct, (uint)start, (uint)count)
+                                                                   .ContinueWith<IEnumerable<ITrackViewModel>>(t =>
+                                                                   {
+                                                                       if (t.IsFaulted)
+                                                                       {
+                                                                           this.fetchState.SetError();
+
+                                                                           var ex = t.Exception.GetBaseException();
+                                                                           System.Diagnostics.Debug.WriteLine($"Failed to get album tracklist. {ex}");
+
+                                                                           throw ex;
+                                                                       }
+
+                                                                       var items = t.Result.Select(x => new TrackViewModel(x, ETrackLHSMode.Artwork));
+
+                                                                       bool hasContents = this.tracklist.Count > 0 || items.Any();
+
+                                                                       if (hasContents)
+                                                                       {
+                                                                           this.fetchState.SetAvailable();
+                                                                       }
+                                                                       else
+                                                                       {
+                                                                           this.fetchState.SetEmpty();
+                                                                       }
+
+                                                                       return items;
+
+                                                                   }, ct, TaskContinuationOptions.NotOnCanceled | TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default));
+
                     break;
 
                 default:
                     throw new InvalidOperationException("Unknown tracklist type specified.");
             }
-
-
-            tracksTask.ContinueWith(t =>
-            {
-                if (t.IsFaulted)
-                {
-                    this.fetchState.SetEmpty();
-
-                    var ex = t.Exception.GetBaseException();
-                    System.Diagnostics.Debug.WriteLine($"Failed to fetch tracklist. {ex}");
-
-                    throw ex;
-                }
-
-
-                var tracks = t.Result.Select(x => new TrackViewModel(x, this.Type == ETracklistType.Album ? ETrackLHSMode.Number
-                                                                                                          : ETrackLHSMode.Artwork));
-
-                this.tracklist.SetContents(tracks);
-
-                if (this.tracklist.Count == 0)
-                {
-                    this.fetchState.SetEmpty();
-                }
-                else
-                {
-                    this.fetchState.SetAvailable();
-                }
-
-
-            }, this.tokenSource.Token, TaskContinuationOptions.NotOnCanceled | TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
         }
 
 
