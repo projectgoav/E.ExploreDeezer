fbd5019696afec0b442e06bec58040f7a3fb7a5a
Ewan <git@ewan.emccartney.co.uk>
2020-04-20 14:50:58 +0100

    Re-arrange some of the header contents for Artists and Tracklist views.

---
+++ E.ExploreDeezer.Core/Common/UserOverview/UserOverviewDataController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using E.Deezer;
+using E.Deezer.Api;
+
+using E.ExploreDeezer.Core.ViewModels;
+using E.ExploreDeezer.Core.Extensions;
+using E.ExploreDeezer.Core.Collections;
+
+namespace E.ExploreDeezer.Core.Common
+{
+    internal interface IUserOverviewDataController
+    {
+        /*
+        IObservableCollection<IAlbumViewModel> FavouriteAlbums { get; }
+        event FetchStateChangedEventHandler OnFavouriteAlbumsFetchStateChanged;
+
+        IObservableCollection<IArtistViewModel> FavouriteArtists { get; }
+        event FetchStateChangedEventHandler OnFavouriteArtistsFetchStateChanged;
+
+        IObservableCollection<ITrackViewModel> FavouriteTracks { get; }
+        event FetchStateChangedEventHandler OnFavouriteTracksFetchStateChanged;
+
+        IObservableCollection<IPlaylistViewModel> UserPlaylists { get; }
+        event FetchStateChangedEventHandler OnUserPlaylistFetchStateChanged;
+
+        IObservableCollection<IPlaylistViewModel> FavouritePlaylists { get; }
+        event FetchStateChangedEventHandler OnFavouritePlaylistsFetchStateChanged;
+        */
+    
+        // Flow?
+        // Followers  / Following...
+    }
+
+    internal class UserOverviewDataController : IUserOverviewDataController,
+                                                IDisposable
+    {
+        private readonly IDeezerSession session;
+        private readonly UpdatableFetchState userPlaylistFetchState;
+        private readonly UpdatableFetchState favouriteAlbumFetchState;
+        private readonly UpdatableFetchState favouriteTrackFetchState;
+        private readonly UpdatableFetchState favouriteArtistFetchState;
+        private readonly UpdatableFetchState favouritePlaylistFetchState;
+        private readonly PagedObservableCollection<IAlbumViewModel> favouriteAlbums;
+        private readonly PagedObservableCollection<ITrackViewModel> favouriteTracks;
+        private readonly PagedObservableCollection<IArtistViewModel> favouriteArtists;
+        private readonly FixedSizeObservableCollection<IPlaylistViewModel> userPlaylists;
+        private readonly FixedSizeObservableCollection<IPlaylistViewModel> favouritePlaylists;
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+
+            }
+        }
+    }
+}
---
+++ E.ExploreDeezer.Core/Common/UserOverview/UserOverviewViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E.ExploreDeezer.Core.Common
+{
+    class UserOverviewViewModel
+    {
+    }
+}
